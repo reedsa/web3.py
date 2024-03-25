e5ab0360c5ccd350f4af4368e56fb6ab68ed790d
Stuart Reed <stuart.reed@ethereum.org>
2024-03-25 12:35:18 -0600

    Fix typing, remove @curry

--- web3/_utils/events.py
+++ web3/_utils/events.py
@@ -29,6 +29,7 @@ from eth_abi.codec import (
 from eth_typing import (
     ChecksumAddress,
     HexStr,
+    Primitives,
     TypeStr,
 )
 from eth_utils import (
@@ -99,9 +100,8 @@ if TYPE_CHECKING:
     )
 
 
-@curry
 def _log_entry_data_to_bytes(
-    log_entry_data: Union[HexStr, str, bytes, bytearray],
+    log_entry_data: Union[Primitives, HexStr, str],
 ) -> bytes:
     return hexstr_if_str(to_bytes, log_entry_data)
 
@@ -236,24 +236,24 @@ def get_event_data(
     elif not log_entry["topics"]:
         raise MismatchedABI("Expected non-anonymous event to have 1 or more topics")
     elif event_abi_to_log_topic(dict(event_abi)) != _log_entry_data_to_bytes(
-        log_entry["topics"][0]
+        log_entry["topics"][0].hex()
     ):
         raise MismatchedABI("The event signature did not match the provided ABI")
     else:
         log_topics = log_entry["topics"][1:]
 
-    log_topics = [_log_entry_data_to_bytes(topic) for topic in log_topics]
+    log_topics_bytes = [_log_entry_data_to_bytes(topic.hex()) for topic in log_topics]
     log_topics_abi = get_indexed_event_inputs(event_abi)
     log_topic_normalized_inputs = normalize_event_input_types(log_topics_abi)
     log_topic_types = get_event_abi_types_for_decoding(log_topic_normalized_inputs)
     log_topic_names = get_abi_input_names(ABIEvent({"inputs": log_topics_abi}))
 
-    if len(log_topics) != len(log_topic_types):
+    if len(log_topics_bytes) != len(log_topic_types):
         raise LogTopicError(
-            f"Expected {len(log_topic_types)} log topics.  Got {len(log_topics)}"
+            f"Expected {len(log_topic_types)} log topics.  Got {len(log_topics_bytes)}"
         )
 
-    log_data = _log_entry_data_to_bytes(log_entry["data"])
+    log_data = _log_entry_data_to_bytes(log_entry["data"].hex())
     log_data_abi = exclude_indexed_event_inputs(event_abi)
     log_data_normalized_inputs = normalize_event_input_types(log_data_abi)
     log_data_types = get_event_abi_types_for_decoding(log_data_normalized_inputs)
@@ -279,7 +279,7 @@ def get_event_data(
 
     decoded_topic_data = [
         abi_codec.decode([topic_type], topic_data)[0]
-        for topic_type, topic_data in zip(log_topic_types, log_topics)
+        for topic_type, topic_data in zip(log_topic_types, log_topics_bytes)
     ]
     normalized_topic_data = map_abi_data(
         BASE_RETURN_NORMALIZERS, log_topic_types, decoded_topic_data
